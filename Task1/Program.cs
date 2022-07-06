b477b8cbdbc22c1512682d79ead827b60dd13960
Aleksandr <maivan007@yandex.ru>
2022-07-06 21:58:04 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿void Zadacha19()
+{
+    int number = 16361;
+    int digitOnes = number%10;
+    int digitTens = number/10 %10;
+    int digitThousands = number/1000 %10;
+    int digitTenOfThousands = number/10000;
+
+    if(digitOnes == digitTenOfThousands && digitTens == digitThousands)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+
+Zadacha19();
