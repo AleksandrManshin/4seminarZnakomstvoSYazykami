0b4b79e612830731907a3dc5b8c1b6c82cb9fa0d
Aleksandr <maivan007@yandex.ru>
2022-07-06 22:52:53 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿void Zadacha26()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Количество знаков в числе {number} равно {MethodSRaschetom(number)}");
+
+
+int MethodSRaschetom(int number)
+{   
+    int count = 1;
+    while(number/10 > 0)
+    {
+        number = number/10;
+        count++;
+    }
+    return count;
+}
+}
+
+Zadacha26();
