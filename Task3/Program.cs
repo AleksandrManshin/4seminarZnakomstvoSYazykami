cb0206492591bf4106bba02812dfb20c6f1c33f0
Aleksandr <maivan007@yandex.ru>
2022-07-07 20:40:24 +0400

    Add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿void Zadacha28()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Произведение всех чисел от 1 до {number} равна " + Raschet(number));
+}
+
+int Raschet(int numb)
+{
+    int sum = 1;
+    for (int i = 1; i <= numb; i++)
+    {
+        sum = sum * i;
+ 
+    }
+    return sum;
+}
+
+Zadacha28();
