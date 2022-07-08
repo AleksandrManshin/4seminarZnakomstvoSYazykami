c05945ba3b557e02a7c52529f4dc0dd7aed4bcce
Aleksandr <maivan007@yandex.ru>
2022-07-08 23:31:32 +0400

    Add Track 4

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿void Zadacha30()
+{   
+    int size = 8;
+    int [] array = new int [size];
+    array = FillArrayFunction(size);
+    PrintArray(array);
+}
+
+int [] FillArrayFunction(int size)
+{
+    int [] fillArray = new int [size];
+    Random random = new Random();
+    for(int i = 0; i < fillArray.Length; i++)
+    {
+        fillArray[i] = random.Next(0, 2);
+    }
+    return fillArray;
+}
+
+void PrintArray(int [] array)
+{
+    Console.WriteLine("Вывод массива: ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array [i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Zadacha30();
